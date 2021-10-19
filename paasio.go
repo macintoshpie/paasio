b051c37ac52608798ac766f96f757493079fde7a
Ted Summer <ted.summer2@gmail.com>
2021-10-18 18:21:52 -0600

    feat: init read and writer constructors

--- paasio.go
+++ paasio.go
@@ -1,1 +1,43 @@
 package paasio
+
+import "io"
+
+type readCounter struct {
+	io.Reader
+}
+
+type writeCounter struct {
+	io.Writer
+}
+
+type readWriteCounter struct {
+	io.ReadWriter
+}
+
+func (rc readCounter) ReadCount() (int64, int) {
+	return 0, 0
+}
+
+func (wc writeCounter) WriteCount() (int64, int) {
+	return 0, 0
+}
+
+func (rwc readWriteCounter) ReadCount() (int64, int) {
+	return 0, 0
+}
+
+func (rwc readWriteCounter) WriteCount() (int64, int) {
+	return 0, 0
+}
+
+func NewReadCounter(reader io.Reader) ReadCounter {
+	return readCounter{reader}
+}
+
+func NewWriteCounter(writer io.Writer) WriteCounter {
+	return writeCounter{writer}
+}
+
+func NewReadWriteCounter(readWriter io.ReadWriter) ReadWriteCounter {
+	return readWriteCounter{readWriter}
+}
